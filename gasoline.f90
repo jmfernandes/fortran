7d27b091d4e2ec0060012624224ce90728da02b4
Josh Fernandes <joshfernandes@mac.com>
2013-09-11 01:19:45 -0700

    made some changes

--- gasoline.f90
+++ gasoline.f90
@@ -1,6 +1,19 @@
 !program to calculate amount of money spent on gas
 program gasoline
 	implicit none    ! Require all variables to be explicitly declared
-	interactive_loop: do
-	end do interactive_loop
+	INTEGER :: Class
+	class = 2
+	SELECT CASE (Class)
+   		CASE (1)
+      		WRITE(*,*)  'Freshman'
+   		CASE (2)
+      		WRITE(*,*)  'Sophomore'
+   		CASE (3)
+      		WRITE(*,*)  'Junior'
+   		CASE (4)
+      		WRITE(*,*)  'Senior'
+   		CASE DEFAULT
+      		WRITE(*,*)  "Hmmmm, I don't know"
+		END SELECT
+		WRITE(*,*)  'Done'
 end program gasoline
