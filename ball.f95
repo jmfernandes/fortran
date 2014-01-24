801b7df2812f0a8fd9717f483070a978b086e85d
Josh Fernandes <joshfernandes@mac.com>
2014-01-23 17:30:06 -0800

    working on ball program

--- ball.f95
+++ ball.f95
@@ -13,6 +13,40 @@ program ball_path
 
 	use setup
 	implicit none
-	real(dp) :: t, dt, x, y, vx, vy
+	real(dp) :: t, dt, x, y, vx, vy, vx0, vy0
+
+	!!define variables
+	h0 = 100._dp
+	v0 = 15._dp
+	alpha = 36*(pi/180) !convert from degrees to radians
+	
+	t = 0._dp
+	dt = 0.01_dp
+
+	x0 = 0._dp
+	y0 = h0
+
+	vx0 = v0*cos(alpha)
+	vy0 = v0*sin(alpha)
+	
+
+	!!calculate
+
+	do while (y >= 0._dp)
+
+		vx = vx0
+		vy = vy0 - g*t
+		x = vx*t
+		y = y0 + vy0*t - g/2 * t**2
+
+		print *, t, x
+		t = t + dt
+
+	end do
+
+
+
+
+
 
 end program ball_path
