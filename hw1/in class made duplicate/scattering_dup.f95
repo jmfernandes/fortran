d5d4abeccac82141e5503466075b3f232a7a7b9c
Josh Fernandes <joshfernandes@mac.com>
2014-01-30 17:36:25 -0800

    rewrote the scattering code using in class examples

--- hw1/in class made duplicate/scattering_dup.f95
+++ hw1/in class made duplicate/scattering_dup.f95
@@ -4,46 +4,115 @@ module setup
 
 	use NumType
 	implicit none
-	real(dp) :: E, dE, E0, gamma(4)
-	REAL, DIMENSION(:, :), ALLOCATABLE :: T
-	integer :: i, j, N, M, DeAllocateStatus, AllocateStatus, Emax
-	
+	real(dp), parameter :: e0 = 5._dp, emax = 10._dp
+	integer, parameter :: imax = 500
 
 end module setup
 
-program ball_path 
+program transmit
 
 	use setup
 	implicit none
+	real(dp) :: e, de, gam, t1, t2, t3, t4
+	integer :: i 
+	real(dp), external :: tcoeff
+
+	de = emax/imax
+
+	do i = 0, imax
+		e = de*i
+
+		gam = 1._dp
+		t1 = tcoeff(e,gam)
+
+		gam = 0.5_dp
+		t2 = tcoeff(e,gam)
+
+		gam = 0.1_dp
+		t3 = tcoeff(e,gam)
+
+		gam = 0.01_dp
+		t4 = tcoeff(e,gam)
+
+		print *, e, t1, t2, t3, t4
+
+	end do
+
+
+end program transmit
+
+
+function tcoeff(e,gam) result(t)
+
+	use NumType
+	use setup
+	implicit none
+
+	real(dp) :: e, gam, t
+
+	t = (gam/2)**2 / ( (e-e0)**2 + (gam/2)**2 )
+
+end function tcoeff
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+! 	real(dp) :: E, dE, E0, gamma(4)
+! 	REAL, DIMENSION(:, :), ALLOCATABLE :: T
+! 	integer :: i, j, N, M, DeAllocateStatus, AllocateStatus, Emax
 	
-	gamma  = (/ 1.0, 0.5, 0.1, 0.01/) !set the values of gamma
 
-	E = 0._dp		!this is min E value	
-	dE = 0.01_dp	!this is the step size
-	Emax = 10_dp	!this is max E value
+! end module setup
 
-	E0 = 5_dp		!this is a constant
-	j = 0._dp		
+! program ball_path 
 
-	!calculate the size of the array based on how many gammas, and how many steps I am going to take
-	N = size(gamma)
-	M = (Emax/dE)+1
+! 	use setup
+! 	implicit none
+	
+! 	gamma  = (/ 1.0, 0.5, 0.1, 0.01/) !set the values of gamma
 
-	!google told me I had to do this to allocate space
-	ALLOCATE ( T(N, M), STAT = AllocateStatus)
-	IF (AllocateStatus /= 0) STOP "*** Not enough memory ***"
+! 	E = 0._dp		!this is min E value	
+! 	dE = 0.01_dp	!this is the step size
+! 	Emax = 10_dp	!this is max E value
+
+! 	E0 = 5_dp		!this is a constant
+! 	j = 0._dp		
+
+! 	!calculate the size of the array based on how many gammas, and how many steps I am going to take
+! 	N = size(gamma)
+! 	M = (Emax/dE)+1
+
+! 	!google told me I had to do this to allocate space
+! 	ALLOCATE ( T(N, M), STAT = AllocateStatus)
+! 	IF (AllocateStatus /= 0) STOP "*** Not enough memory ***"
 		
-	do while (E < Emax)
-		j = j + 1
-		!this creates a 4 column array of the T data. Each column is for a different value of gamma
-		do i=1,4
-			T(i,j) = (.25*(gamma(i)**2))/((E-E0)**2 + .25*(gamma(i)**2))
-		end do
-		!print the data
-		print *, E, T(1,j), T(2,j), T(3,j), T(4,j)
-		!iterate E
-		E = E + DE
-	end do 
+! 	do while (E < Emax)
+! 		j = j + 1
+! 		!this creates a 4 column array of the T data. Each column is for a different value of gamma
+! 		do i=1,4
+! 			T(i,j) = (.25*(gamma(i)**2))/((E-E0)**2 + .25*(gamma(i)**2))
+! 		end do
+! 		!print the data
+! 		print *, E, T(1,j), T(2,j), T(3,j), T(4,j)
+! 		!iterate E
+! 		E = E + DE
+! 	end do 
 		
-	DEALLOCATE (T, STAT = DeAllocateStatus)
-end program ball_path
+! 	DEALLOCATE (T, STAT = DeAllocateStatus)
+! end program ball_path
---
+++ hw1/in class made duplicate/setup.mod
@@ -0,0 +1,52 @@
+GFORTRAN module version '10' created from scattering_dup.f95
+MD5:59c62291007229d9033b5cab1e497d74 -- If you edit this, you'll get what you deserve.
+
+(() () () () () () () () () () () () () () () () () () () () () () ()
+() () () ())
+
+()
+
+()
+
+()
+
+()
+
+()
+
+(2 '__convert_i4_r8' '(intrinsic)' '' 1 ((PROCEDURE UNKNOWN-INTENT
+UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 FUNCTION ELEMENTAL PURE) (REAL 8 0 0 0
+REAL ()) 0 0 () () 2 () () () 0 0)
+3 'atan' '(intrinsic)' '' 1 ((PROCEDURE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN UNKNOWN 0 0 FUNCTION) (REAL 4 0 0 0 REAL ()) 0 0 () () 3 () () ()
+0 0)
+4 'dp' 'numtype' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (CONSTANT (
+INTEGER 4 0 0 0 INTEGER ()) 0 '8') () 0 () () () 0 0)
+5 'e0' 'setup' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+IMPLICIT-SAVE 0 0) (REAL 8 0 0 0 REAL ()) 0 0 () (CONSTANT (REAL 8 0 0 0
+REAL ()) 0 '0.50000000000000@1') () 0 () () () 0 0)
+6 'emax' 'setup' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+IMPLICIT-SAVE 0 0) (REAL 8 0 0 0 REAL ()) 0 0 () (CONSTANT (REAL 8 0 0 0
+REAL ()) 0 '0.a0000000000000@1') () 0 () () () 0 0)
+7 'iic' 'numtype' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+IMPLICIT-SAVE 0 0) (COMPLEX 8 0 0 0 COMPLEX ()) 0 0 () (CONSTANT (
+COMPLEX 8 0 0 0 COMPLEX ()) 0 '0.00000000000000@0' '0.10000000000000@1')
+() 0 () () () 0 0)
+8 'imax' 'setup' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (CONSTANT (
+INTEGER 4 0 0 0 INTEGER ()) 0 '500') () 0 () () () 0 0)
+9 'kind' '(intrinsic)' '' 1 ((PROCEDURE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN UNKNOWN 0 0 FUNCTION) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () () 9 ()
+() () 0 0)
+10 'numtype' 'numtype' '' 1 ((MODULE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+UNKNOWN 0 0) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () () 0 () () () 0 0)
+11 'pi' 'numtype' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+IMPLICIT-SAVE 0 0) (REAL 8 0 0 0 REAL ()) 0 0 () (CONSTANT (REAL 8 0 0 0
+REAL ()) 0 '0.3243f6a8885a30@1') () 0 () () () 0 0)
+12 'setup' 'setup' '' 1 ((MODULE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+UNKNOWN 0 0) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () () 0 () () () 0 0)
+)
+
+('__convert_i4_r8' 0 2 'atan' 0 3 'dp' 0 4 'e0' 0 5 'emax' 0 6 'iic' 0 7
+'imax' 0 8 'kind' 0 9 'numtype' 0 10 'pi' 0 11 'setup' 0 12)
