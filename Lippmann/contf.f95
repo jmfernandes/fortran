2b22945a6e2c28a6b89325744be6ab7edb09c7e7
jmfernandes <joshfernandes@mac.com>
2014-09-22 15:52:02 -0700

    updated lippman program

--- Lippmann/contf.f95
+++ Lippmann/contf.f95
@@ -1,64 +1,203 @@
 
 program exp_cf_rec
 
-    use NumType
-    implicit none
-    integer 					::	n
-    integer,    parameter       ::  n_basis=5
-    real(dp),   parameter		::  mass=1.0_dp, hbar=2.0_dp,&
-                                	omega_h=1._dp,omega_b=1._dp
-    real(dp) ::     x_mat(0:n_basis,0:n_basis),     &
-                    p_mat(0:n_basis,0:n_basis),     &
-                    h_mat(0:n_basis,0:n_basis)
+	use NumType
+	implicit none
+
+	!=========================== CONSTANTS ===========================
+
+	integer,	parameter	::	n_basis =	5
+	real(dp),   parameter   ::  mass	=	1.0_dp,				& 
+								hbar	=	1.0_dp,				&
+								omega_h	=	1.0_dp,				&
+								omega_b	=	1.0_dp
+
+	!=========================== MATRICES ===========================
+
+	real(dp)                ::  x_mat(0:n_basis,0:n_basis),     &
+								p_mat(0:n_basis,0:n_basis),     &
+								h_mat(0:n_basis,0:n_basis),     &
+								h0_mat(0:n_basis,0:n_basis),    &
+								v_mat(0:n_basis,0:n_basis),     &
+								e_mat(0:n_basis,0:n_basis),     &
+								g0_mat(0:n_basis,0:n_basis)
+
+	!================================================================
+
+	integer,    parameter   ::  lwork=(n_basis+2)*n_basis
+	integer                 ::  n,i,j, block_size, info
+	real(dp)                ::  w_eigen(n_basis),work(lwork)
 
 
 
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    x_mat   = 0._dp
-    p_mat   = 0._dp
-    h_mat   = 0._dp
-
-
-
-    do n=0,n_basis
-        x_mat(n,n) = (hbar/(2*mass*omega_b))*(2*n+1)
-        if (n>=2) then
-            x_mat(n,n-2) = hbar/(2*mass*omega_b)*sqrt(real(n*(n-1)))
-        end if
-        if (n<=(n_basis-2)) then
-            x_mat(n,n+2) = hbar/(2*mass*omega_b)*sqrt(real((n+1)*(n+2)))
-        end if
-    end do
-
-    do n=0,n_basis
-        p_mat(n,n) = (hbar*mass*omega_b)/2*(2*n+1)
-        if (n>=2) then
-            p_mat(n,n-2) = -(hbar*mass*omega_b)/2*sqrt(real(n*(n-1)))
-        end if
-        if (n<=(n_basis-2)) then
-            p_mat(n,n+2) = -(hbar*mass*omega_b)/2*sqrt(real((n+1)*(n+2)))
-        end if
-    end do
-
-    h_mat(0:n_basis,0:n_basis) = p_mat(0:n_basis,0:n_basis)/(2*mass)+mass*omega_h**2*x_mat(0:n_basis,0:n_basis)/2
+	x_mat	=	0._dp
+	p_mat	=	0._dp
+	h_mat	=	0._dp
 
 
 
-    print *, 'x^2 matrix-------------------------'
-    do n=0,n_basis
-        print '(12f10.2)', x_mat(n,0:n_basis)
-    end do
+	do n=0,n_basis
+		x_mat(n,n) = (hbar/(2*mass*omega_b))*(2*n+1)
+		if (n>=2) then
+			x_mat(n,n-2) = hbar/(2*mass*omega_b)*sqrt(real(n*(n-1)))
+		end if
+		if (n<=(n_basis-2)) then
+			x_mat(n,n+2) = hbar/(2*mass*omega_b)*sqrt(real((n+1)*(n+2)))
+		end if
+	end do
 
-    print *, 'p^2 matrix-------------------------'
-    do n=0,n_basis
-        print '(12f10.2)', p_mat(n,0:n_basis)
-    end do
+	do n=0,n_basis
+		p_mat(n,n) = (hbar*mass*omega_b)/2*(2*n+1)
+		if (n>=2) then
+			p_mat(n,n-2) = -(hbar*mass*omega_b)/2*sqrt(real(n*(n-1)))
+		end if
+		if (n<=(n_basis-2)) then
+			p_mat(n,n+2) = -(hbar*mass*omega_b)/2*sqrt(real((n+1)*(n+2)))
+		end if
+	end do
 
-    print *, 'hamiltonian matrix-------------------------'
-    do n=0,n_basis
-        print '(12f10.2)', h_mat(n,0:n_basis)
-    end do
+	h_mat(0:n_basis,0:n_basis) = p_mat(0:n_basis,0:n_basis)/(2*mass)+ &
+							mass*omega_h**2*x_mat(0:n_basis,0:n_basis)/2
+
+
+
+	print *, 'x^2 matrix-------------------------'
+	do n=0,n_basis
+		print '(12f10.2)', x_mat(n,0:n_basis)
+	end do
+
+	print *, 'p^2 matrix-------------------------'
+	do n=0,n_basis
+		print '(12f10.2)', p_mat(n,0:n_basis)
+	end do
+
+	print *, 'hamiltonian matrix-------------------------'
+	do n=0,n_basis
+		print '(12f10.2)', h_mat(n,0:n_basis)
+	end do
+
+!============================================================================
+
+!   compute eigenvalues
+
+	call dsyev('V','U',n_basis+1,h_mat,n_basis+1,w_eigen,work,lwork,info)
+!     call dgeev('V','V',n_basis+1,h_mat,n_basis+1,wr,wi,vl,ldvl,vr,ldvr,work,lwork,info)
+
+	print *, 'info -------------------------'
+	print *, info
+
+	print *, 'eigenvalues-------------------------'
+	print '(10f10.2)', w_eigen
+
+	print *, 'vectors matrix-------------------------'
+	do n=0,n_basis
+		print '(12f10.2)', h_mat(n,0:n_basis)
+	end do
+
+!============================================================================
+
+	block_size = 2
+
+	!build V matrix
+	v_mat(0:n_basis,0:n_basis) = h_mat(0:n_basis,0:n_basis)
+
+	do n=0,n_basis,block_size
+		do i=0,(block_size-1)
+			do j=0,(block_size-1)
+				v_mat(n+i,n+j) = 0
+			end do
+		end do
+	end do
+
+!     print *, 'v matrix-------------------------'
+!     do n=0,n_basis
+!         print '(12f10.0)', v_mat(n,0:n_basis)
+!     end do
+
+	!Build H0 matrix
+	do i=0,n_basis
+		do j=0,n_basis
+			h0_mat(i,j)=h_mat(i,j)-v_mat(i,j)
+		end do    
+	end do
+
+!============================================================================
+
+	e_mat(0:n_basis,0:n_basis)	=	0._dp
+
+	g0_mat(0:n_basis,0:n_basis)	=	inv(e_mat(0:n_basis,0:n_basis)-&
+									h0_mat(0:n_basis,0:n_basis))
+
+!     print *, 'g0 matrix-------------------------'
+!     do n=0,n_basis
+!         print '(12f10.2)', g0_mat(n,0:n_basis)
+!     end do
+
+!=======================================================================
+
+	contains
+
+		! Returns the inverse of a matrix calculated by finding the LU
+		! decomposition.  Depends on LAPACK.
+
+		function inv(A) result(Ainv)
+		  real(dp), dimension(0:n_basis,0:n_basis), intent(in) :: A
+		  real(dp), dimension(0:n_basis,0:n_basis) :: Ainv
+
+		  real(dp), dimension(size(A,1)) :: work  ! work array for LAPACK
+		  integer, dimension(size(A,1)) :: ipiv   ! pivot indices
+		  integer :: n, info
+
+
+		  ! External procedures defined in LAPACK
+		  external DGETRF
+		  external DGETRI
+
+		  ! Store A in Ainv to prevent it from being overwritten by LAPACK
+		  Ainv = A
+		  n = size(A,1)
+
+		  ! DGETRF computes an LU factorization of a general M-by-N matrix A
+		  ! using partial pivoting with row interchanges.
+		  call DGETRF(n, n, Ainv, n, ipiv, info)
+
+		  if (info /= 0) then
+			 stop 'Matrix is numerically singular!'
+		  end if
+
+		  ! DGETRI computes the inverse of a matrix using the LU factorization
+		  ! computed by DGETRF.
+		  call DGETRI(n, Ainv, n, ipiv, work, n, info)
+
+		  if (info /= 0) then
+			 stop 'Matrix inversion failed!'
+		  end if
+		end function inv
+
+		function multiply_mat(n,mult_mat,psi_mat) result(ss)
+		
+			implicit none
+			integer :: n,i
+			complex(dp) ::  mult_mat(0:n_basis,0:n_basis),&
+							res(0:n_basis,0:n_basis),&
+							ss(0:n_basis),&
+							psi_mat(0:n_basis)
+			
+			!create placeholder matrix that will overwrite itself
+			res(0:n_basis,0:n_basis) = mult_mat(0:n_basis,0:n_basis)
+
+			!multiply (G0*V) with itself for n times (n is input)
+			do i=1,n
+			res(0:n_basis,0:n_basis) = matmul(res(0:n_basis,0:n_basis),&
+				mult_mat(0:n_basis,0:n_basis))
+			end do         
+		
+			!return the result as {(G0*V)^n}*psi
+			ss(0:n_basis)=matmul(res(0:n_basis,0:n_basis),psi_mat(0:n_basis))
+
+		end function multiply_mat
 
 end program exp_cf_rec
 
